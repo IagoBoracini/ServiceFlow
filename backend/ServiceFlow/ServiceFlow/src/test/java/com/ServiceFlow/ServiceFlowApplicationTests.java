8e1d91c83ff98b9a3e0649d078dd40eb38dc4b47
Iago Boracini <iago.bacaia@gmai.com>
2026-07-28 00:13:52 -0300

    chore: cria estrutura inicial do projeto

---
+++ backend/ServiceFlow/ServiceFlow/src/test/java/com/ServiceFlow/ServiceFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ServiceFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
